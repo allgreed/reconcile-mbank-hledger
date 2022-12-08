a6fcc1ab7bc4b92b84422868ab4e97aa03858f32
Olgierd "Allgreed" Kasprowicz <olgierd@kasprowicz.pro>
2022-12-08 17:55:00 +0100

    Assert equivalence

--- test_main.py
+++ test_main.py
@@ -7,7 +7,13 @@ m = MbankTransaction(amount=5, description="ble", accounting_date="2022-10-02")
 h = HledgerTransaction(amount=5, description="ble", ledger_id=5)
 m5 = MbankTransaction(amount=5, description="fuj", accounting_date="2022-10-02")
 h5 = HledgerTransaction(amount=5, description="fuj", ledger_id=6)
+m7 = MbankTransaction(amount=7, description="bork", accounting_date="2022-10-02")
+h7 = HledgerTransaction(amount=7, description="bork", ledger_id=7)
 
+@pytest.mark.parametrize("additional_m,additional_h", [
+    (set(), set()),
+    ({m7}, {h7}),
+])
 @pytest.mark.parametrize("mbank_transactions,hledger_transactions,result", [
     (set(), set(), []),
     ({m}, set(), [TransactionsMatch(mbank_transactions=[m])]),
@@ -16,7 +22,9 @@ h5 = HledgerTransaction(amount=5, description="fuj", ledger_id=6)
     ({m, m5}, {h}, [TransactionsMatch(mbank_transactions={m5, m}, hledger_transactions={h})]),
     ({m}, {h, h5}, [TransactionsMatch(mbank_transactions={m}, hledger_transactions={h, h5})]),
 ])
-def test_find_unbalanched_matches(mbank_transactions, hledger_transactions, result):
+def test_find_unbalanched_matches(mbank_transactions, hledger_transactions, result, additional_h, additional_m):
+    mbank_transactions.union(additional_m)
+    hledger_transactions.union(additional_h)
     assert result == find_unbalanced_matches(mbank_transactions=mbank_transactions, hledger_transactions=hledger_transactions)
     legacy_find_unbalanced_matches(mbank_transactions=mbank_transactions, hledger_transactions=hledger_transactions)
 
